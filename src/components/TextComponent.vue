e252ddd5b51921f9b4386840208c64b5f4f24959
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-18 15:23:31 +0900

    draw text

--- src/components/TextComponent.vue
+++ src/components/TextComponent.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { Font, load } from "opentype.js";
+import { Font, load, parse } from "opentype.js";
 import { onMounted, ref } from "vue";
 
 const props = defineProps<{
@@ -19,6 +19,16 @@ onMounted(function () {
     }
     if (!font) {
       return;
+    }
+    const size = parseInt(props.fontSize);
+    const f = parse(font.toArrayBuffer());
+    const ctx = el.value.getContext("2d");
+    for (const t of props.text) {
+      const g = f.charToGlyph(t);
+      if (!g.advanceWidth) {
+        continue;
+      }
+      g.draw(ctx, 0, 0, size);
     }
   });
 });

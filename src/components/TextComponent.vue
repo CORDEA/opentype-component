ade0d18ee267cf44e5bbeae8f8528c787427805f
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-18 15:20:40 +0900

    load font

--- src/components/TextComponent.vue
+++ src/components/TextComponent.vue
@@ -3,10 +3,25 @@
 </template>
 
 <script setup lang="ts">
+import { Font, load } from "opentype.js";
+import { onMounted, ref } from "vue";
+
 const props = defineProps<{
   text: string;
   fontSize: string;
 }>();
+
+const el = ref();
+onMounted(function () {
+  load("font.ttf", function (err: any, font?: Font) {
+    if (err) {
+      return;
+    }
+    if (!font) {
+      return;
+    }
+  });
+});
 </script>
 
 <style scoped></style>

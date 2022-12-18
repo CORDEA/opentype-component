f46ae86a189fbcb57a41a3fd72a450cea1758dd7
Yoshihiro Tanaka <contact@cordea.jp>
2022-12-18 15:19:50 +0900

    add a component

---
+++ src/components/TextComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <canvas ref="el"></canvas>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  text: string;
+  fontSize: string;
+}>();
+</script>
+
+<style scoped></style>
